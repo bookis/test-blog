f08133e91864f62445fc6d03dd7165b78052c806
Bookis Smuin <bookis.smuin@gmail.com>
2014-10-06 15:43:16 -0700

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   get "/posts", to: "posts#index"
-  
+
+  root "posts#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
