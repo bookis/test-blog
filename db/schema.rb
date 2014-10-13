7e950cf9f2bf792e667275aa8ff9e01d45294a37
Bookis Smuin <bookis.smuin@gmail.com>
2014-10-13 11:24:31 -0700

    remove pw stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141010174248) do
+ActiveRecord::Schema.define(version: 20141007182145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 20141010174248) do
     t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "password_digest"
   end
 
 end
