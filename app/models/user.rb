8cc7c057616585ea1d51816f642420b282cda72a
Bookis Smuin <bookis.smuin@gmail.com>
2014-10-14 10:54:47 -0700

    add post validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :posts, through: :comments
   has_secure_password
-  
+  validates :username, presence: true, length: {minimum: 1}
 end
